9c7bae1c2a785995e63e406f35d2bff7182309dc
Hamid <25hamid04@gmail.com>
2023-01-16 16:38:53 +0100

    description

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,32 +1,19 @@
 'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-
-
 /*
  * Complete the 'fizzBuzz' function below.
  *
  * The function accepts INTEGER n as parameter.
  */
+
+
+// If i is a multiple of both 3 and 5, print FizzBuzz.
+// If i is a multiple of 3 (but not 5), print Fizz.
+// If i is a multiple of 5 (but not 3), print Buzz.
+// If i is not a multiple of 3Â or 5, print the value of i.
+
+
+
 
 // This following Code is my solution
 
@@ -48,11 +35,5 @@ function fizzBuzz(n) {
 
 }
 
-// ^
 
-function main() {
-    const n = parseInt(readLine().trim(), 10);
-
-    fizzBuzz(n);
-}
-
+fizzBuzz(15);
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# FizzBuzz-Hackerrank-
+# FizzBuzz (Hackerrank)
 This is my Solution to the FizzBuzz Test, which i found at Hackerrank.
+
+
+I deleted hackerrank testcases etc.
+
+Given a number n, for each integer i in the range from 1 to n inclusive,
+print one value per line as follows:
+    If i is a multiple of both 3 and 5, print FizzBuzz.
+    If i is a multiple of 3 (but not 5), print Fizz.
+    If i is a multiple of 5 (but not 3), print Buzz.
+    If i is not a multiple of 3Â or 5, print the value of i.
